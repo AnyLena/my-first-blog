b5601ea55915422f608c35330a460fa9f2159fd8
LenaBultmann <lena.bultmann@gmx.de>
2023-12-19 15:15:45 +0100

    adding navbar structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
+import Navbar from './components/Navbar'
 
 function App() {
   return (
-    <>
-      <div>
+    <div className="App">
+      <Navbar />
+      <div className="content">
         <h1>Component Template</h1>
       </div>
-    </>
+    </div>
   );
 }
 
