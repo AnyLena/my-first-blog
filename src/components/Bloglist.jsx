b942b959fc9e142d3e3917ebd4021c84d42a650e
LenaBultmann <lena.bultmann@gmx.de>
2023-12-29 12:05:04 +0100

    creating multiple components

--- src/components/Bloglist.jsx
+++ src/components/Bloglist.jsx
@@ -1,10 +1,11 @@
-const Bloglist = ({blogs}) => {
+const Bloglist = ({blogs , title}) => {
 
   return (
     <div className="blog-list">
+      <h2 className="blog-heading"> {title} </h2>
       {blogs.map((blog) => (
         <div className="blog-preview" key={blog.id}>
-          <h2>{blog.title}</h2>
+          <h2>{blogs.title}</h2>
           <p>
             Written by <em>{blog.author}</em>
           </p>
