8b2b3ee352b7d8826629bef6464ba406cae05dd4
LenaBultmann <lena.bultmann@gmx.de>
2023-12-31 16:09:08 +0100

    adding loading message

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,20 +3,24 @@ import Bloglist from "./Bloglist";
 
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
+  const [isPending, setIsPending] = useState(true);
 
   useEffect( () => {
     fetch('http://localhost:8000/blogs')
     .then(res => {
+      console.log(res)
       return res.json();
     })
     .then(data => {
       setBlogs(data);
+      setIsPending(false)
     })
   },[])
 
    
   return (
     <div className="home">
+    {isPending && <div>Loading...</div>}  
     {blogs && <Bloglist blogs={blogs} title="All Blogs!"/>}
     {blogs && <Bloglist blogs={blogs.filter( (blog) => blog.author === "Chatty")} title="Chatty's Blogs!"/>}
     </div>
