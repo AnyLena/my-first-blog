67f6d29b39b16aa45d186b70060a235d7bff9d50
LenaBultmann <lena.bultmann@gmx.de>
2023-12-31 16:36:33 +0100

    adding error messages

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,25 +4,38 @@ import Bloglist from "./Bloglist";
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
   const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
-  useEffect( () => {
-    fetch('http://localhost:8000/blogs')
-    .then(res => {
-      console.log(res)
-      return res.json();
-    })
-    .then(data => {
-      setBlogs(data);
-      setIsPending(false)
-    })
-  },[])
+  useEffect(() => {
+    fetch("http://localhost:8000/blogs")
+      .then((res) => {
+        if (!res.ok) {
+          throw Error("Could not fetch the data for that resource");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setBlogs(data);
+        setIsPending(false);
+        setError(null)
+      })
+      .catch((err) => {
+        setIsPending(false)
+        setError(err.message);
+      });
+  }, []);
 
-   
   return (
     <div className="home">
-    {isPending && <div>Loading...</div>}  
-    {blogs && <Bloglist blogs={blogs} title="All Blogs!"/>}
-    {blogs && <Bloglist blogs={blogs.filter( (blog) => blog.author === "Chatty")} title="Chatty's Blogs!"/>}
+      {error && <div> {error} </div>}
+      {isPending && <div>Loading...</div>}
+      {blogs && <Bloglist blogs={blogs} title="All Blogs!" />}
+      {blogs && (
+        <Bloglist
+          blogs={blogs.filter((blog) => blog.author === "Chatty")}
+          title="Chatty's Blogs!"
+        />
+      )}
     </div>
   );
 };
